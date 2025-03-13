00ab3c3643b8c39bdad4aa981e40401756d532ba
Andrey Kapturin <andruxa21097@gmail.com>
2025-03-13 18:03:47 +0700

    feat: Установлен и настроен express и mysql

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import 'dotenv/config';
-import { startServer } from './src/express.js';
-startServer();
+import express from 'express';
+import { router } from './src/router/index.js';
+const PORT = process.env.PORT ?? 3000;
+
+const app = express();
+app.use(router);
+
+app.listen(PORT, () => console.log(`Server start on ${PORT} port`));
