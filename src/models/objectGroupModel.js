294e376ce357608e9108c176e3aa1893aceeaf45
Andrey Kapturin <andruxa21097@gmail.com>
2025-03-14 10:09:25 +0700

    bugfix: Синтаксис SQL запросов заменен на snake case

--- src/models/objectGroupModel.js
+++ src/models/objectGroupModel.js
@@ -2,7 +2,7 @@ import { connection } from '../database/index.js';
 
 export const getAll = async () => {
   try {
-    const [ result ] = await connection.query('SELECT * FROM objectsGroups');
+    const [ result ] = await connection.query('SELECT * FROM objects_groups');
     return result;
   } catch (error) {
     console.log('Object groups request from BD error', error);
