b974ae886542959a3cb23f05c2db055a68eb786b
Andrey Kapturin <andruxa21097@gmail.com>
2025-03-28 16:26:37 +0700

    build: Замена MySQL на SQLite

--- src/controllers/objectsGroupsController.js
+++ src/controllers/objectsGroupsController.js
@@ -1,45 +1,46 @@
-import { connection } from "../database/index.js";
+import { DB } from "../database/index.js";
 
-export const getObjectsGroupsList = async (req, res) => {
+export const getObjectsGroupsList = (req, res) => {
   try {
-    const [ objectsGroups ] = await connection.query('SELECT id, name FROM objects_groups');
+    const objectsGroups = DB.prepare('SELECT id, name FROM objects_groups').all();
     res.json(objectsGroups);
   } catch (error) {
-    res.status(500).send({ error: 'Something failed!' });
+    console.error(error)
+    res.status(500).json({ message: 'Ошибка сервера при получении списка групп объектов' });
   }
 }
 
-export const getObjectsGroupById = async (req, res) => {
+export const getObjectsGroupById = (req, res) => {
   const objectGroupId = Number(req.params.id);
 
   if (Number.isNaN(objectGroupId)) {
-    res.status(400).send({ message: 'Параметр "id" имеет неверный формат или отсутствует' });
+    res.status(400).json({ message: 'Параметр "id" имеет неверный формат или отсутствует' });
     return;
   }
 
   try {
-    const [ rows ] = await connection.query('SELECT * FROM objects_groups WHERE id = ?', objectGroupId);
-    const objectsGroup = rows[0];
+    const objectsGroup = DB.prepare('SELECT * FROM objects_groups WHERE id = ?').get(objectGroupId);
 
     if (!objectsGroup) {
       res.status(404).send({ message: `Группа объектов с id ${objectGroupId} не найдена` });
       return;
     }
 
-    const [ metrics ] = await connection.query(`SELECT m.* FROM metrics m LEFT JOIN objects_groups_metrics ogm ON ogm.objects_group_id = ? WHERE ogm.metric_id = m.id`, objectGroupId);
-
-    const metricsWithIndicators = await Promise.all(metrics.map(async (metric) => {
-      const [ indicators ] = await connection.query(
+    const metrics = DB.prepare('SELECT m.* FROM metrics m LEFT JOIN objects_groups_metrics ogm ON ogm.objects_group_id = ? WHERE ogm.metric_id = m.id')
+    .all(objectGroupId)
+    .map((metric) => {
+      const indicators = DB.prepare(
         `SELECT 
           mi.id, mi.text, miv.value
           FROM metric_indicators mi
           LEFT JOIN metric_indicators_values miv ON miv.metric_indicator_id = mi.id AND miv.objects_group_id = ?
-          WHERE mi.metric_id = ? AND miv.value IS NOT NULL;`, [objectGroupId, metric.id]);
+          WHERE mi.metric_id = ? AND miv.value IS NOT NULL;`)
+      .all(objectGroupId, metric.id);
       metric.indicators = indicators;
       return metric;
-    }));
+    });
 
-    objectsGroup.metrics = metricsWithIndicators;
+    objectsGroup.metrics = metrics;
     res.status(200).json(objectsGroup);
   } catch (error) {
     console.error(error)
@@ -47,85 +48,97 @@ export const getObjectsGroupById = async (req, res) => {
   }
 }
 
-export const addMetricToObjectsGroup = async (req, res) => {
+export const addMetricToObjectsGroup = (req, res) => {
   const objectGroupId = Number(req.params.id);
   const metric = req.body;
-
+  
   if (Number.isNaN(objectGroupId)) {
-    res.status(400).send({ message: 'Параметр "id" имеет неверный формат или отсутствует'});
+    res.status(400).json({ message: 'Параметр "id" имеет неверный формат или отсутствует'});
     return;
   }
 
   try {
-    await connection.beginTransaction();
-    await connection.query('INSERT INTO objects_groups_metrics (objects_group_id, metric_id) VALUES (?, ?)', [objectGroupId, metric.id]);
-    await Promise.all(metric.indicators.map(async ({ id, value })=> {
-      await connection.query('INSERT INTO metric_indicators_values (objects_group_id, metric_indicator_id, value) VALUES (?, ?, ?)', [objectGroupId, id, value])
-    }));
-    await connection.commit();
+    const addMetricTransaction = DB.transaction((objectGroupId, metricId, indicators) => {
+      DB.prepare('INSERT INTO objects_groups_metrics (objects_group_id, metric_id) VALUES (?, ?)').run(objectGroupId, metricId);
+      indicators.forEach(({ id, value }) => {
+        DB.prepare('INSERT INTO metric_indicators_values (objects_group_id, metric_indicator_id, value) VALUES (?, ?, ?)').run(objectGroupId, id, value);
+      });
+    });
+
+    addMetricTransaction(objectGroupId, metric.id, metric.indicators);
+
     res.sendStatus(201);
   } catch (error) {
-    await connection.rollback();
     console.error('Ошибка сервера при добавлении метрики в группу объекта', error);
-    res.status(500).send({ message: 'Ошибка сервера при добавлении метрики в группу объекта'})
+    res.status(500).json({ message: 'Ошибка сервера при добавлении метрики в группу объекта' })
   }
 }
 
-export const updateIndicatorsValuesForObjectsGroup = async (req, res) => {
+export const updateIndicatorsValuesForObjectsGroup = (req, res) => {
   const objectGroupId = Number(req.params.id);
   const indicators = req.body;
 
   if (Number.isNaN(objectGroupId)) {
-    res.status(400).send({ message: 'Параметр "id" имеет неверный формат или отсутствует'});
+    res.status(400).json({ message: 'Параметр "id" имеет неверный формат или отсутствует'});
     return;
   }
 
   try {
-    await Promise.all(indicators.map(async ({ id, value })=> {
-      return await connection.query('INSERT INTO metric_indicators_values (objects_group_id, metric_indicator_id, value) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE value = ?;', [objectGroupId, id, value, value]);
-    }));
+    const updateIndicatorsTransaction = DB.transaction((objectGroupId, indicators) => {
+      indicators.forEach(({ id, value }) => {
+        DB.prepare('INSERT OR REPLACE INTO metric_indicators_values (objects_group_id, metric_indicator_id, value) VALUES (?, ?, ?);')
+        .run(objectGroupId, id, value);
+      });
+    })
+
+    updateIndicatorsTransaction(objectGroupId, indicators);
+    
     res.sendStatus(204);
   } catch (error) {
     console.error('Ошибка сервера при обновлении значений метрик', error);
-    res.status(500).send({ message: 'Ошибка сервера при обновлении значений метрик'})
+    res.status(500).json({ message: 'Ошибка сервера при обновлении значений метрик'})
   }
 }
 
-export const deleteMetricFromObjectsGroup = async (req, res) => {
+export const deleteMetricFromObjectsGroup = (req, res) => {
   const objectGroupId = Number(req.params.objectsGroupId);
   const metricId = Number(req.params.metricId);
 
   if (Number.isNaN(objectGroupId) || Number.isNaN(metricId)) {
-    res.status(400).send({ message: 'Параметр "objectGroupId" или "metricId" имеет неверный формат или отсутствует'});
+    res.status(400).json({ message: 'Параметр "objectGroupId" или "metricId" имеет неверный формат или отсутствует'});
     return;
   }
 
   try {
-    await connection.beginTransaction();
-    const [ { affectedRows: metricsRows } ] = await connection.query('DELETE FROM objects_groups_metrics WHERE objects_group_id = ? AND metric_id = ?', [ objectGroupId, metricId ]);
-    const [ { affectedRows: valuesRows } ] = await connection.query('DELETE FROM metric_indicators_values WHERE objects_group_id = ? AND metric_indicator_id IN (SELECT id FROM metric_indicators WHERE metric_id = ?)', [ objectGroupId, metricId ]);
+    const deleteMetricTransaction = DB.transaction((objectGroupId, metricId) => {
+      const { changes: metricRows } = DB.prepare('DELETE FROM objects_groups_metrics WHERE objects_group_id = ? AND metric_id = ?')
+      .run(objectGroupId, metricId);
+      const { changes: valuesRows } = DB.prepare('DELETE FROM metric_indicators_values WHERE objects_group_id = ? AND metric_indicator_id IN (SELECT id FROM metric_indicators WHERE metric_id = ?)')
+      .run(objectGroupId, metricId);
 
-    if (metricsRows === 0 || valuesRows === 0) {
-      await connection.rollback();
-      res.status(404).json({ message: 'Неверные параметры группы объектов или метрики'});
+      return metricRows === 0 && valuesRows === 0;
+    })
+
+    const isNotDeleted = deleteMetricTransaction(objectGroupId, metricId);
+
+    if (isNotDeleted) {
+      res.status(404).json({ message: `Нет записей в таблицах для метрики с id ${metricId} у группы с id ${objectGroupId}` });
       return;
     }
-
-    await connection.commit();
+    
     res.sendStatus(204);
   } catch (error) {
-    await connection.rollback()
     console.error('Ошибка сервера при удалении метрики у группы объектов', error);
-    res.status(500).json({ message: 'Ошибка сервера при удалении метрики у группы объектов'})
+    res.status(500).json({ message: 'Ошибка сервера при удалении метрики у группы объектов' });
   }
 }
 
-export const updateObjectsGroup = async (req, res) => {
+export const updateObjectsGroup = (req, res) => {
   const objectGroupId = Number(req.params.id);
   const valuesForUpdate = req.body;
 
   if (Number.isNaN(objectGroupId)) {
-    res.status(400).send({ message: 'Параметр "id" имеет неверный формат или отсутствует' });
+    res.status(400).json({ message: 'Параметр "id" имеет неверный формат или отсутствует' });
     return;
   }
 
@@ -138,10 +151,9 @@ export const updateObjectsGroup = async (req, res) => {
 
     values.push(objectGroupId);
 
-    const sql = `UPDATE objects_groups SET ${setTemplates.join(', ')} WHERE id = ?`;
-    const [ { affectedRows } ] = await connection.query(sql, values);
+    const { changes } = DB.prepare(`UPDATE objects_groups SET ${setTemplates.join(', ')} WHERE id = ?`).run(values);
 
-    if (affectedRows === 0) {
+    if (changes === 0) {
       res.status(404).json({ message: `Группа с id ${objectGroupId} не найдена` });
       return
     }
