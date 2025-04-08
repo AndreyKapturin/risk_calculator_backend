e1ec16c88be373ef0ec47d65626d699b6948b3c1
Andrey Kapturin <andruxa21097@gmail.com>
2025-04-08 10:43:59 +0700

    feat: Единый обработчик ошибок. Валидация

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Стек
 - Express
-- MySQL
+- SQLite
--- src/controllers/metricsController.js
+++ src/controllers/metricsController.js
@@ -1,64 +1,21 @@
-import * as MetricModel from '../services/metricService.js';
+import * as MetricService from '../services/metricService.js';
 
 export const createMetric = (req, res) => {
-  try {
-    const { type, name, indicators } = req.body;
-    const metricId = MetricModel.create(type, name, indicators);
-    res.status(201).json({ id: metricId });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Ошибка сервера при создании метрики' });
-  }
+  const metricId = MetricService.create(req.body.metric);
+  res.status(201).json({ id: metricId });
 }
 
 export const getAllMetricsWithIndicators = (req, res) => {
-  try {
-    const metrics = MetricModel.getAllWithIndicators();
-    res.status(200).json(metrics);
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ message: 'Ошибка сервера при получении метрик' });
-  }
+  const metrics = MetricService.getAllWithIndicators();
+  res.status(200).json(metrics);
 }
 
 export const updateMetric = (req, res) => {
-  const metricId = Number(req.params.id);
-  const dataForUpdate = req.body;
-
-  if (Number.isNaN(metricId)) {
-    res.status(400).json({ message: 'Параметр "id" имеет неверный формат или отсутствует' });
-    return;
-  }
-  
-  try {
-    const isUpdated = MetricModel.update(metricId, dataForUpdate);
-
-    if (!isUpdated) {
-      res.status(404).json({ message: 'Метрика не найдена' });
-      return;
-    }
-
-    res.sendStatus(200);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Ошибка сервера при обновлении метрики' });
-  }
+  MetricService.update(metricId, req.body.data);
+  res.sendStatus(200);
 }
 
 export const addIndicatorToMetric = (req, res) => {
-  const metricId = Number(req.params.id);
-  const { text } = req.body;
-
-  if (Number.isNaN(metricId)) {
-    res.status(400).json({ message: 'Параметр "id" имеет неверный формат или отсутствует' });
-    return;
-  }
-  
-  try {
-    const indicatorId = MetricModel.addIndicator(metricId, text);
-    res.status(201).json({ id: indicatorId, text });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Ошибка сервера при обновлении метрики' });
-  }
+  const indicatorId = MetricService.addIndicator(req.params.id, req.body.indicator);
+  res.status(201).json({ id: indicatorId });
 }
