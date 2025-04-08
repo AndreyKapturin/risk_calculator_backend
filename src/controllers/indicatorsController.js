b8f6aae1c3dd10fc9a2dcf7556c2ee7ba658bd2c
Andrey Kapturin <andruxa21097@gmail.com>
2025-04-08 10:43:59 +0700

    feat: Единый обработчик ошибок. Валидация

--- src/controllers/indicatorsController.js
+++ src/controllers/indicatorsController.js
@@ -1,48 +1,11 @@
-import * as MetricIndicatorModel from "../services/metricIndicatorService.js";
+import * as MetricIndicatorService from "../services/metricIndicatorService.js";
 
 export const updateIndicatorText = (req, res) => {
-  const indicatorId = Number(req.params.id);
-  const { text } = req.body;
-
-  if (Number.isNaN(indicatorId)) {
-    res.status(400).json({ message: 'Параметр "id" имеет неверный формат или отсутствует' });
-    return;
-  }
-
-  try {
-    const isUpdated = MetricIndicatorModel.update(indicatorId, text);
-
-    if (!isUpdated) {
-      res.status(404).json({ message: `Нет записей в таблицах для индикатора с id ${indicatorId}` });
-      return
-    }
-
-    res.sendStatus(204);
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Ошибка сервера при обновлении индикатора' });
-  }
+  MetricIndicatorService.update(req.params.id, req.body.indicator);
+  res.sendStatus(204);
 }
 
 export const deleteIndicator = (req, res) => {
-  const indicatorId = Number(req.params.id);
-  
-  if (Number.isNaN(indicatorId)) {
-    res.status(400).json({ message: 'Параметр "id" имеет неверный формат или отсутствует' });
-    return;
-  }
-  
-  try {
-    const isDeleted = MetricIndicatorModel.deleteIndicator(indicatorId);
-    
-    if (!isDeleted) {
-      res.status(404).json({ message: `Нет записей в таблицах для индикатора с id ${indicatorId}` });
-      return;
-    }
-
-    res.sendStatus(204);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Ошибка сервера при удалении метрики' });
-  }
+  MetricIndicatorService.deleteIndicator(req.params.id);
+  res.sendStatus(204);
 }
--- src/services/objectsGroupService.js
+++ src/services/objectsGroupService.js
@@ -1,8 +1,15 @@
 import { DB } from "../database/index.js";
 import * as MetricService from './metricService.js';
+import { ErrorWithStatusCode } from "../utils/ErrorWithStatusCode.js";
 
 export const getAllForList = () => {
   return DB.prepare('SELECT id, name FROM objects_groups').all();
+}
+
+export const getObjectsGroupById = (objectsGroupId) => {
+  const objectsGroup = DB.prepare(`SELECT * FROM objects_groups WHERE id = ?`).get(objectsGroupId);
+  if (!objectsGroup) throw new ErrorWithStatusCode(404, `Группа объектов с id ${objectsGroupId} не найдена`);
+  return objectsGroup;
 }
 
 export const getObjectsGroupWithMetricsById = (objectsGroupId) => {
@@ -14,24 +21,24 @@ export const getObjectsGroupWithMetricsById = (objectsGroupId) => {
     material_damage_potencial_score as materialDamagePotencialScore
     FROM objects_groups WHERE id = ?`).get(objectsGroupId);
 
-  if (!objectsGroup) {
-    return null;
-  }
+  if (!objectsGroup) throw new ErrorWithStatusCode(404, `Группа объектов с id ${objectsGroupId} не найдена`);
 
   objectsGroup.metrics = MetricService.getAllWithIndicatorsForObjectsGroup(objectsGroup.id);
   return objectsGroup;
 }
 
 export const addMetric = (objectGroupId, metric) => (DB.transaction((objectGroupId, metric) => {
-    DB.prepare('INSERT INTO objects_groups_metrics (objects_group_id, metric_id) VALUES (?, ?)')
-      .run(objectGroupId, metric.id);
-    
-    const setValueToMetricIndicatorQuery = DB.prepare('INSERT INTO metric_indicators_values (objects_group_id, metric_indicator_id, value) VALUES (?, ?, ?)');
-    
-    metric.indicators.forEach(({ id, value }) => {
-        setValueToMetricIndicatorQuery.run(objectGroupId, id, value);
-    });
-  })
+  if (Number.isNaN(Number(metric.id))) throw new ErrorWithStatusCode(400, 'В теле запроса параметр "id" должен быть числом');
+  if (!Array.isArray(metric.indicators)) throw new ErrorWithStatusCode(400, 'В теле запроса параметр "indicators" должен быть массивом');
+
+  DB.prepare('INSERT INTO objects_groups_metrics (objects_group_id, metric_id) VALUES (?, ?)').run(objectGroupId, metric.id);
+  
+  const setValueToMetricIndicatorQuery = DB.prepare('INSERT INTO metric_indicators_values (objects_group_id, metric_indicator_id, value) VALUES (?, ?, ?)');
+  
+  metric.indicators.forEach(({ id, value }) => {
+      setValueToMetricIndicatorQuery.run(objectGroupId, id, value);
+  });
+})
 )(objectGroupId, metric);
 
 export const updateIndicatorsValues = (objectGroupId, indicators) => (DB.transaction((objectGroupId, indicators) => {
@@ -43,7 +50,7 @@ export const updateIndicatorsValues = (objectGroupId, indicators) => (DB.transac
     return totalChanges;
   }, 0);
 
-  return changes > 0;
+  if (changes === 0) throw new ErrorWithStatusCode(404, `Значения индикаторов для группы объектов с id ${objectGroupId} не найдены`);
 }))(objectGroupId, indicators);
 
 export const deleteMetric = (objectGroupId, metricId) => (DB.transaction((objectGroupId, metricId) => {
@@ -55,11 +62,19 @@ export const deleteMetric = (objectGroupId, metricId) => (DB.transaction((object
     WHERE objects_group_id = ? AND metric_indicator_id IN (SELECT id FROM metric_indicators WHERE metric_id = ?)`)
     .run(objectGroupId, metricId);
 
-  return metricRows === 0 || valuesRows === 0;
+  if (metricRows === 0 && valuesRows === 0) throw new ErrorWithStatusCode(404, `Нет записей в таблицах для метрики с id ${metricId} у группы с id ${objectGroupId}`);
 }))(objectGroupId, metricId);
 
 export const update = (objectGroupId, dataForUpdate) => {
-  const { values, setTemplates } = Object.entries(dataForUpdate).reduce((res, [ key, value ]) => {
+  const keysAndValues = Object.entries(dataForUpdate);
+  if (keysAndValues.length === 0) throw new ErrorWithStatusCode(400, 'Нет данных для обновления');
+  // вспомогательная функция для фильтрации ключей и типов значений входящего объекта
+  // не пропустить параметры не входящие в состав таблицы
+  // проверять соответствие типов
+  
+  getObjectsGroupById(objectGroupId);
+
+  const { values, setTemplates } = keysAndValues.reduce((res, [ key, value ]) => {
     res.values.push(value);
     res.setTemplates.push(`${key} = ?`);
     return res;
@@ -68,5 +83,5 @@ export const update = (objectGroupId, dataForUpdate) => {
   values.push(objectGroupId);
 
   const { changes } = DB.prepare(`UPDATE objects_groups SET ${setTemplates.join(', ')} WHERE id = ?`).run(values);
-  return changes > 0;
+  if (changes === 0) throw new ErrorWithStatusCode(404, `Значения метрики для группы с id ${objectGroupId} не обновлены`);
 }
